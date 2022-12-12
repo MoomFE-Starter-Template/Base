091ab2aed8ec2bc50d699d4159a5afe9014613e6
倚窗听雨丿醉无心 <994039348@qq.com>
2022-12-12 15:29:35 +0800

    更改 critters 样式预加载策略, 解决可能发生的样式优先级问题

--- vite.config.ts
+++ vite.config.ts
@@ -106,6 +106,7 @@ export default defineConfig(({ mode }) => {
       crittersOptions: {
         inlineFonts: false,
         preloadFonts: false,
+        preload: 'swap',
       },
       includedRoutes: paths => paths.filter(path => !path.includes(':')),
       onFinished: () => {
