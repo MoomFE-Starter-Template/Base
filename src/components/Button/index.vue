0ba0e82bef76eef6d231c38c3c0fb8292fcdad49
倚窗听雨丿醉无心 <994039348@qq.com>
2023-05-25 15:13:25 +0800

    优化按钮在禁用时的样式

--- src/components/Button/index.vue
+++ src/components/Button/index.vue
@@ -3,7 +3,7 @@
     class="
       px-4 py-1 rounded
       text-white
-      bg-primary hover:bg-primary-hover active:bg-primary-active disabled:bg-gray-400
+      bg-primary hover:bg-primary-hover active:bg-primary-active disabled:bg-gray-400!
     "
     :disabled="props.disabled"
     @click="emit('click')"
