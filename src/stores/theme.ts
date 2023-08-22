fd14aad87df0eaee82e2990ac2cbb8ea4615283a
倚窗听雨丿醉无心 <994039348@qq.com>
2023-08-22 16:07:12 +0800

    优化主题管理相关

--- src/stores/theme.ts
+++ src/stores/theme.ts
@@ -1,28 +1,29 @@
+import type { BasicColorSchema } from '@vueuse/core';
 import { defineStore } from 'pinia';
 
 export const useThemeStore = defineStore('theme', () => {
   /** 主题模式状态 */
-  const { store, state } = useColorMode({
+  const mode = useColorMode<BasicColorSchema>({
     storageKey: 'st-color-scheme',
     initialValue: 'light',
   });
 
-  /** 是否是深色模式 */
-  const dark = computed({
-    get: () => state.value === 'dark',
-    set: v => (store.value = v ? 'dark' : 'light'),
+  /** 主题切换 */
+  const { state, next } = useCycleList<BasicColorSchema>(['light', 'dark', 'auto'], {
+    initialValue: mode.store,
   });
 
-  /** 切换主题 */
-  function toggle() {
-    if (store.value === 'auto') store.value = 'light';
-    else if (store.value === 'light') store.value = 'dark';
-    else if (store.value === 'dark') store.value = 'auto';
-  }
+  /** 是否是深色模式 */
+  const dark = computed({
+    get: () => mode.value === 'dark',
+    set: v => (mode.value = v ? 'dark' : 'light'),
+  });
+
+  syncRef(mode.store, state);
 
   return {
-    value: store,
+    value: mode.store,
     dark,
-    toggle,
+    toggle: next,
   };
 });
