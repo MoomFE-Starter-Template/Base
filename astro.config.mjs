8170c8a8a30ad00a68cbfcc854257ebf744763e5
倚窗听雨丿醉无心 <994039348@qq.com>
2024-07-02 19:40:34 +0800

    项目添加 vue 集成

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,12 @@
 import { defineConfig } from 'astro/config';
+import Vue from '@astrojs/vue';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    Vue({
+      jsx: true,
+      devtools: true,
+    }),
+  ],
+});
