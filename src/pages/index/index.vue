e4429152d1dd3eaabe056188e74b7f52fcd2f57b
倚窗听雨丿醉无心 <994039348@qq.com>
2024-07-04 08:53:10 +0800

    不支持 Vue 那种的动态路由, game over

---
+++ src/pages/index/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <div w-62.5 mxa mt-6>
+    <ElInput
+      v-model="name"
+      placeholder="你的名字是？"
+    />
+  </div>
+
+  <div mt-3>
+    <ElButton type="primary" :disabled="!name" @click="go">确定</ElButton>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+  import { ElButton, ElInput } from 'element-plus';
+  import 'element-plus/theme-chalk/index.css';
+
+  const name = ref('');
+
+  function go() {
+    window.location.href = `/hello/${name.value}`;
+  }
+</script>
