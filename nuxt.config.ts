f7babafae131d7ad4ca0bd92d595aa26deb6c8e4
倚窗听雨丿醉无心 <994039348@qq.com>
2024-07-23 16:16:02 +0800

    项目添加 unocss 集成

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,24 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: [
+    '@unocss/nuxt',
+  ],
+
+  css: [
+    '@unocss/reset/tailwind-compat.css',
+  ],
+
+  features: {
+    // 针对 UnoCSS
+    inlineStyles: false,
+  },
+
+  devtools: {
+    enabled: true,
+  },
+
   compatibilityDate: '2024-04-03',
   future: {
     compatibilityVersion: 4,
-  },
-  devtools: {
-    enabled: true,
   },
 });
---
+++ unocss.config.ts
@@ -0,0 +1,41 @@
+import { defineConfig, presetAttributify, presetIcons, presetUno, transformerDirectives, transformerVariantGroup } from 'unocss';
+import { presetExtra } from 'unocss-preset-extra';
+
+export default defineConfig({
+  theme: {
+    // 断点
+    breakpoints: {
+      'xs': '480px',
+      'sm': '576px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
+    // 过渡持续时间
+    duration: {
+      colors: '300ms',
+    },
+  },
+  presets: [
+    // 默认预设, 和 Tailwind 类似
+    presetUno(),
+    // 属性模式
+    presetAttributify({
+      prefix: 'un:',
+    }),
+    // 图标预设
+    presetIcons(),
+    // 类名简写及额外一些样式预设
+    presetExtra(),
+  ],
+  transformers: [
+    // 在 CSS 中使用 @apply 指令
+    transformerDirectives(),
+    // 变体组功能
+    transformerVariantGroup(),
+  ],
+  blocklist: [
+    'container',
+  ],
+});
