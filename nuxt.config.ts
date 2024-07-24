6103d5902f22dd1241470b1626717055d0c584af
倚窗听雨丿醉无心 <994039348@qq.com>
2024-07-24 15:25:14 +0800

    项目添加 unplugin-icons 集成

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,28 @@
+import { dirname, resolve } from 'node:path';
+import { fileURLToPath } from 'node:url';
+import Components from 'unplugin-vue-components/vite';
+import IconsResolver from 'unplugin-icons/resolver';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@unocss/nuxt',
+    ['unplugin-icons/nuxt', { scale: 1 }],
   ],
+
+  vite: {
+    plugins: [
+      Components({
+        dts: resolve(__dirname, './app/components.d.ts'),
+        dirs: [],
+        resolvers: [
+          IconsResolver(),
+        ],
+      }),
+    ],
+  },
 
   css: [
     '@unocss/reset/tailwind-compat.css',
