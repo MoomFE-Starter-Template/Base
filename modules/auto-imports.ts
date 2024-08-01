383a79738b7bb9205ac3373fc65a7d0df05ef1cf
倚窗听雨丿醉无心 <994039348@qq.com>
2024-08-01 11:49:54 +0800

    自动为状态管理目录下的文件添加热更新代码

--- modules/auto-imports.ts
+++ modules/auto-imports.ts
@@ -1,8 +1,12 @@
 import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import { defineNuxtModule } from '@nuxt/kit';
+import { normalizePath } from 'vite';
+import { addVitePlugin, defineNuxtModule } from '@nuxt/kit';
+import { parseModule } from 'magicast';
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const storesPath = normalizePath(resolve(__dirname, '../app/stores'));
 
 export default defineNuxtModule({
   hooks: {
@@ -13,11 +17,35 @@ export default defineNuxtModule({
         global: true,
       });
     },
-    // 添加 API 自动导入目录
+    // 添加状态管理目录的自动导入
     'imports:dirs': function (dirs) {
-      dirs.push(
-        resolve(__dirname, '../app/stores'),
-      );
+      dirs.push(storesPath);
     },
+  },
+  setup() {
+    // 自动为状态管理目录下的文件添加热更新代码
+    // https://pinia.vuejs.org/zh/cookbook/hot-module-replacement.html
+    addVitePlugin(() => {
+      return {
+        name: 'stores-auto-hmr',
+        transform(code, id) {
+          if (!id.startsWith(storesPath) || !/\.tsx?$/.test(id)) return;
+
+          const mod = parseModule(code);
+          const exports = Object.keys(mod.exports);
+
+          if (!exports.length) return;
+
+          code = `${code}\n
+if (import.meta.hot){
+${
+  exports.map(name => `  import.meta.hot.accept(acceptHMRUpdate(${name}, import.meta.hot));`)
+}
+}`;
+
+          return code;
+        },
+      };
+    });
   },
 });
