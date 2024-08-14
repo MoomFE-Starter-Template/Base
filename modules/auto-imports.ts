8633d2e3a4be9e2e93aeafc59b68dd8ff1fd548f
倚窗听雨丿醉无心 <994039348@qq.com>
2024-08-14 08:35:19 +0800

    删除无用的状态管理目录导入及相关功能

--- modules/auto-imports.ts
+++ modules/auto-imports.ts
@@ -1,11 +1,4 @@
-import { fileURLToPath } from 'node:url';
-import { dirname, resolve } from 'pathe';
-import { addVitePlugin, defineNuxtModule } from 'nuxt/kit';
-import { parseModule } from 'magicast';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
-const storesPath = resolve(__dirname, '../app/stores');
+import { defineNuxtModule } from 'nuxt/kit';
 
 export default defineNuxtModule({
   hooks: {
@@ -16,35 +9,5 @@ export default defineNuxtModule({
         global: true,
       });
     },
-    // 添加状态管理目录的自动导入
-    'imports:dirs': function (dirs) {
-      dirs.push(storesPath);
-    },
-  },
-  setup() {
-    // 自动为状态管理目录下的文件添加热更新代码
-    // https://pinia.vuejs.org/zh/cookbook/hot-module-replacement.html
-    addVitePlugin(() => {
-      return {
-        name: 'stores-auto-hmr',
-        transform(code, id) {
-          if (!id.startsWith(storesPath) || !/\.tsx?$/.test(id)) return;
-
-          const mod = parseModule(code);
-          const exports = Object.keys(mod.exports);
-
-          if (!exports.length) return;
-
-          code = `${code}\n
-if (import.meta.hot){
-${
-  exports.map(name => `  import.meta.hot.accept(acceptHMRUpdate(${name}, import.meta.hot));`)
-}
-}`;
-
-          return code;
-        },
-      };
-    });
   },
 });
