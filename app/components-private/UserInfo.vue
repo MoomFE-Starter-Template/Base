cd6a046e6e99fcb73ecd74ff2a8ff1e38764c711
倚窗听雨丿醉无心 <994039348@qq.com>
2024-08-20 08:16:18 +0800

    优化用户信息展示组件在未登录时的展现

--- app/components-private/UserInfo.vue
+++ app/components-private/UserInfo.vue
@@ -1,26 +1,31 @@
 <!-- 用户信息展示 -->
 
 <template>
-  <ElSkeleton flex="~ items-center gap-2" :loading="auth.info.isLoading" animated>
+  <ElSkeleton flex="~ items-center gap-2" :loading="auth.isLogin ? auth.info.isLoading : false" animated>
     <div flex="~ wrap gap-2" items-center justify-between>
-      <!-- 用户信息请求成功 -->
-      <div v-if="auth.info.isSuccess" flex="~ items-center gap-2">
-        <ElAvatar :size="50" :src="auth.info.data?.avatar" />
-        <div h-full flex="~ col grow">
-          <div>{{ auth.info.data!.name }}</div>
-          <div text-sm op-70>{{ auth.info.data!.username }}</div>
+      <!-- 已登录 -->
+      <template v-if="auth.isLogin">
+        <!-- 用户信息请求成功 -->
+        <div v-if="auth.info.isSuccess" flex="~ items-center gap-2">
+          <ElAvatar :size="50" :src="auth.info.data?.avatar" />
+          <div h-full flex="~ col grow">
+            <div>{{ auth.info.data!.name }}</div>
+            <div text-sm op-70>{{ auth.info.data!.username }}</div>
+          </div>
         </div>
-      </div>
-      <!-- 用户信息请求失败 -->
-      <div v-else class="op-70">用户信息获取失败</div>
-      <!-- 退出登录按钮 -->
-      <ElButton
-        v-if="auth.info.isFinished"
-        size="small" type="danger" text bg :loading="auth.logout.isLoading"
-        @click="auth.logout.execute(true)"
-      >
-        退出登录
-      </ElButton>
+        <!-- 用户信息请求失败 -->
+        <div v-else class="op-70">用户信息获取失败</div>
+        <!-- 退出登录按钮 -->
+        <ElButton
+          v-if="auth.info.isFinished"
+          size="small" type="danger" text bg :loading="auth.logout.isLoading"
+          @click="auth.logout.execute(true)"
+        >
+          退出登录
+        </ElButton>
+      </template>
+      <!-- 未登录 -->
+      <div v-else flex-grow text="center neutral-5">未登录</div>
     </div>
 
     <template #template>
