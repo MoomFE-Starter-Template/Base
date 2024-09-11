a12b004d76baa8e5277659f6c80198074568922a
倚窗听雨丿醉无心 <994039348@qq.com>
2024-09-11 19:41:42 +0800

    添加 i18n 配置文件

--- layers/module-i18n/nuxt.config.ts
+++ layers/module-i18n/nuxt.config.ts
@@ -1,3 +1,8 @@
+import { fileURLToPath } from 'node:url';
+import { dirname, resolve } from 'pathe';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/i18n',
@@ -7,6 +12,7 @@ export default defineNuxtConfig({
     lazy: true,
     strategy: 'no_prefix',
     defaultLocale: 'cn',
+    vueI18n: resolve(__dirname, 'vue-i18n.options.ts'),
   },
   future: {
     compatibilityVersion: 4,
