cff376ad2deb267ea0745c400c6018751faf7218
倚窗听雨丿醉无心 <994039348@qq.com>
2024-10-27 15:22:19 +0800

    请求用户信息时显示 Loading; 请求失败提示刷新

--- app/stores/auth.ts
+++ app/stores/auth.ts
@@ -1,5 +1,9 @@
+import type { FetchError } from 'ofetch';
 import { getUserInfo, logout as toLogout, usernameLogin, type UsernameLoginData } from '@/apis/auth';
 import { accessToken } from '@/shared/env';
+import LoadingIcon from '~icons/svg-spinners/ring-resize';
+import { delay, isPlainObject } from 'mixte';
+import type { ResponseData } from '~/apis/types';
 
 export const useAuthStore = defineStore('auth', () => {
   /** 是否登录 */
@@ -14,6 +18,40 @@ export const useAuthStore = defineStore('auth', () => {
 
   /** 用户信息 */
   const info = useRequestReactive(getUserInfo);
+
+  // 用户信息请求中, 显示 Loading
+  wheneverImmediate(() => info.isLoading, (_, __, onCleanup) => {
+    const loading = ElMessage.info({
+      message: '加载中...',
+      duration: 0,
+      icon: LoadingIcon,
+    });
+
+    onCleanup(() => {
+      loading.close();
+    });
+  });
+
+  // 用户信息请求失败, 并且不是用户鉴权的原因, 提示用户刷新页面
+  info.onError((error: FetchError & { ok: boolean }) => {
+    let data: ResponseData;
+
+    if (error.ok === false || (isPlainObject(data = error.response?._data) && !['40001', '40005'].includes(data.code))) {
+      ElMessageBox.confirm('用户信息加载失败，请刷新页面重试', {
+        title: '提示',
+        confirmButtonText: '刷新页面',
+        closeOnClickModal: false,
+        closeOnPressEscape: false,
+        closeOnHashChange: false,
+        showCancelButton: false,
+        showClose: false,
+        beforeClose: (action, instance) => {
+          instance.confirmButtonLoading = true;
+          location.reload();
+        },
+      });
+    }
+  });
 
   // 登录后获取用户信息
   wheneverImmediate(isLogin, () => {
